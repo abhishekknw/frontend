b97f2caa44b6cf90588931979ff2323ac22ce5c2
yogesh <yogesh.mhetre@machadalo.com>
2018-09-17 20:18:00 +0530

    @Editfeature changepwd - added changepassword feature and integrated with backend

--- ui/buyer_catalogue/app/modules/pages/changePassword/changePassword.controller.js
+++ ui/buyer_catalogue/app/modules/pages/changePassword/changePassword.controller.js
@@ -1,22 +1,22 @@
 angular.module('catalogueApp')
 .controller('changePswdCtrl',
-    ['$scope', '$rootScope', '$window', '$location','societyDetailsViewService','$stateParams','commonDataShare','constants','$timeout','Upload','cfpLoadingBar','permissions',
-    function ($scope, $rootScope, $window, $location, societyDetailsViewService, $stateParams,commonDataShare,constants,$timeout,Upload,cfpLoadingBar, permissions) {
+    ['$scope', '$rootScope', '$window', '$location','changePasswordService','$stateParams','commonDataShare','constants','$timeout','Upload','cfpLoadingBar','permissions',
+    function ($scope, $rootScope, $window, $location, changePasswordService, $stateParams,commonDataShare,constants,$timeout,Upload,cfpLoadingBar, permissions) {
 
 
     console.log("Hello passwrod change World");
 
-
+        console.log($rootScope);
+        $scope.userInfo = $rootScope.globals.userInfo;
+        $scope.model = {};
+        $scope.isValid = false;
+        $scope.passwordError = constants.password_error;
         $scope.changePassword = function(){
-          var data = {
-            password : $scope.userInfo.password,
-          }
-          changePasswordService.changePassword($scope.userInfo.id,data)
+          changePasswordService.changePassword($scope.userInfo.id,$scope.model)
           .then(function onSuccess(response){
             console.log(response);
-            $scope.userInfo = {};
-            commonDataShare.closeModal('#passwordModal');
             swal(constants.name,constants.changePassword_success,constants.success);
+            $location.path("/logout");
           }).catch(function onError(response){
             console.log(response);
             commonDataShare.showErrorMessage(response);
@@ -30,5 +30,6 @@ angular.module('catalogueApp')
             $scope.isValid = true;
           else
             $scope.isValid = false;
+          console.log($scope.isValid,$scope.model);
         }
       }]);
