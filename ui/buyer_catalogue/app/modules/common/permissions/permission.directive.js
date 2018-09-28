d78fd7ed991d334c3cf532e3ac36128179142baf
yogesh <yogesh.mhetre@machadalo.com>
2018-09-28 18:11:17 +0530

    @Newfeature permissions - added permissions at frontend on navigation, if the permission for page in genral_user_permissions table is not set or null or false then the page will be redirected to forbiddenpage

--- ui/buyer_catalogue/app/modules/common/permissions/permission.directive.js
+++ ui/buyer_catalogue/app/modules/common/permissions/permission.directive.js
@@ -7,11 +7,11 @@ angular.module('Authentication')
        },
 
        link: function (scope, elem, attrs) {
-            scope.$watch(AuthService.isLoggedIn, function() {
+            scope.$watch(AuthService.isLoggedIn, function() {              
                 if (AuthService.userHasPermission(scope.permission)) {
                     elem.show();
                 } else {
-                  if(elem[0].tagName == 'BUTTON' || elem[0].tagName == 'INPUT')
+                  if(elem[0].tagName == 'BUTTON' || elem[0].tagName == 'INPUT' || elem[0].tagName == 'SELECT')
                     attrs.$set('disabled', 'disabled');
                   else
                     elem.hide();
