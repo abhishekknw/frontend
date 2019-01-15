a5740c4d6a3ca692f115a750830a794547f82305
Vyoma Desai <vyoma.desai@machaddalo.com>
2019-01-15 17:44:23 +0530

    @Dashboard - added excel donload + Excel-factory

--- ui/buyer_catalogue/app/modules/pages/dashboard/DashboardCtrl.js
+++ ui/buyer_catalogue/app/modules/pages/dashboard/DashboardCtrl.js
@@ -6,7 +6,7 @@
     'use strict';
 
   angular.module('catalogueApp')
-      .controller('DashboardCtrl',function($scope,NgMap, $rootScope, baConfig, colorHelper,DashboardService, commonDataShare, constants,$location,$anchorScroll,uiGmapGoogleMapApi,uiGmapIsReady,Upload,cfpLoadingBar,$stateParams) {
+      .controller('DashboardCtrl',function($scope,NgMap, $rootScope, baConfig, colorHelper,DashboardService, commonDataShare, constants,$location,$anchorScroll,uiGmapGoogleMapApi,uiGmapIsReady,Upload,cfpLoadingBar,$stateParams,$timeout,Excel) {
  $scope.itemsByPage=15;
  $scope.campaign_id = $stateParams.proposal_id;
  $scope.query = "";
@@ -3317,8 +3317,40 @@ $scope.getCampaignWiseSummary = function(){
      return temp_data;
    }
 
+  $scope.exportData = function () {
+  $('#customer1').tableExport({ type: 'csv', escape: 'false' });
+  $('#customer2').tableExport({ type: 'csv', escape: 'false' });
+  $('#customer3').tableExport({ type: 'csv', escape: 'false' });
+  $('#customer4').tableExport({ type: 'csv', escape: 'false' });
+  $('#customer5').tableExport({ type: 'csv', escape: 'false' });
+  $('#customer6').tableExport({ type: 'csv', escape: 'false' });
+
+
+  };
+
+  $scope.exportToExcel=function(tableId){ // ex: '#my-table'
+  var exportHref=Excel.tableToExcel(tableId,'WireWorkbenchDataExport');
+  console.log("hello");
+  $timeout(function(){location.href=exportHref;},100); // trigger download
+  }
 
 //END
 
 })
+
+
 })();
+app.factory('Excel',function($window){
+        var uri='data:application/vnd.ms-excel;base64,',
+            template='<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+            base64=function(s){return $window.btoa(unescape(encodeURIComponent(s)));},
+            format=function(s,c){return s.replace(/{(\w+)}/g,function(m,p){return c[p];})};
+        return {
+            tableToExcel:function(tableId,worksheetName){
+                var table=$(tableId),
+                    ctx={worksheet:worksheetName,table:table.html()},
+                    href=uri+base64(format(template,ctx));
+                return href;
+            }
+        };
+    })
