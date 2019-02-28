a2413982f7869b481e9ec63c05110ff7e3369092
Kshitij Mittal <kshitij.mittal01@gmail.com>
2019-02-28 18:34:51 +0530

    Add entity selection in Base booking create
    Add dropdown to select base entity type, while creating a base booking.
    On base entity type select, populate a table to select entity atributes.
    If users unselects a base entity type attribute, strike it through.
    Allow user to mark required base entity attributes, which were already required.

--- machadalo/src/components/Booking/Base/Create.jsx
+++ machadalo/src/components/Booking/Base/Create.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 import Select from 'react-select';
 
 const AttributeTypes = [
   { value: 'FLOAT', label: 'Float' },
   { value: 'STRING', label: 'Text' },
   { value: 'DROPDOWN', label: 'Dropdown' },
-  { value: 'EMAIL', label: 'Email' }
+  { value: 'EMAIL', label: 'Email' },
+  { value: 'MULTISELECT', label: 'Multi Select' }
+];
+
+const EntityTypes = [
+  { value: 'FLOAT', label: 'Float' },
+  { value: 'STRING', label: 'Text' },
+  { value: 'INVENTORY', label: 'Inventory' },
+  { value: 'INVENTORY_TYPE', label: 'Base Inventory' },
+  { value: 'DROPDOWN', label: 'Dropdown' },
+  { value: 'EMAIL', label: 'Email' },
+  { value: 'BASE_ENTITY_TYPE', label: 'Base Entity Type' }
 ];
 
 // Get attribute type option from string
@@ -13,6 +25,17 @@ const getAttributeTypeOption = value => {
   for (let i = 0, l = AttributeTypes.length; i < l; i += 1) {
     if (AttributeTypes[i].value === value) {
       return AttributeTypes[i];
+    }
+  }
+
+  return { value };
+};
+
+// Get base entity type option from entity type
+const getEntityTypeOption = value => {
+  for (let i = 0, l = EntityTypes.length; i < l; i += 1) {
+    if (EntityTypes[i].value === value) {
+      return EntityTypes[i];
     }
   }
 
@@ -28,14 +51,14 @@ const getRawAttribute = () => {
   };
 };
 
-const getBaseEntityTypeOption = (baseEntityTypeList, baseEntityType) => {
+const getBaseEntityTypeOption = (baseEntityTypeList, baseEntityTypeId) => {
   for (let i = 0, l = baseEntityTypeList.length; i < l; i += 1) {
-    if (baseEntityType === baseEntityTypeList[i].id) {
+    if (baseEntityTypeId === baseEntityTypeList[i].id) {
       return baseEntityTypeList[i];
     }
   }
 
-  return { id: baseEntityType };
+  return { id: baseEntityTypeId };
 };
 
 export default class CreateBaseBooking extends React.Component {
@@ -49,7 +72,9 @@ export default class CreateBaseBooking extends React.Component {
           ...getRawAttribute()
         }
       ],
-      baseEntityType: null
+      entities: [],
+      baseEntityTypeId: null,
+      selectedBaseEntityType: null
     };
 
     this.onAddAttributeClick = this.onAddAttributeClick.bind(this);
@@ -57,11 +82,35 @@ export default class CreateBaseBooking extends React.Component {
     this.onSubmit = this.onSubmit.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleAttributeChange = this.handleAttributeChange.bind(this);
+    this.handleEntityChange = this.handleEntityChange.bind(this);
     this.renderAttributeRow = this.renderAttributeRow.bind(this);
+    this.renderEntityRow = this.renderEntityRow.bind(this);
   }
 
   componentDidMount() {
     this.props.getBaseEntityTypeList();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { baseEntityType: prevBaseEntityType } = prevProps;
+    const { baseEntityType: newBaseEntityType } = this.props;
+
+    if (
+      !this.state.selectedBaseEntityType &&
+      !prevBaseEntityType.currentBaseEntityType &&
+      newBaseEntityType.currentBaseEntityType
+    ) {
+      this.setState({
+        selectedBaseEntityType: newBaseEntityType.currentBaseEntityType,
+        entities: newBaseEntityType.currentBaseEntityType.entity_attributes.map(
+          item => ({
+            ...item,
+            selected: true,
+            allowRequired: item.is_required
+          })
+        )
+      });
+    }
   }
 
   onAddAttributeClick() {
@@ -77,19 +126,28 @@ export default class CreateBaseBooking extends React.Component {
   }
 
   onBaseEntityTypeChange(option) {
-    this.setState({
-      baseEntityType: option.id
-    });
+    this.setState(
+      {
+        baseEntityTypeId: option.id,
+        selectedBaseEntityType: null
+      },
+      () => {
+        this.props.getBaseEntityType(this.state.baseEntityTypeId);
+      }
+    );
   }
 
   onSubmit() {
     // TODO: Submit form data
-    const { name, attributes, baseEntityType } = this.state;
+    const { name, attributes, baseEntityTypeId, entities } = this.state;
 
     const data = {
       name,
       booking_attributes: attributes,
-      base_entity_type_id: baseEntityType
+      base_entity_type_id: baseEntityTypeId,
+      entity_attributes: entities
+        .filter(item => item.selected)
+        .map(item => ({ name: item.name, is_required: item.is_required }))
     };
 
     console.log('data', data);
@@ -108,6 +166,16 @@ export default class CreateBaseBooking extends React.Component {
 
     this.setState({
       attributes
+    });
+  }
+
+  handleEntityChange(entity, index) {
+    const entities = [...this.state.entities];
+
+    entities[index] = entity;
+
+    this.setState({
+      entities
     });
   }
 
@@ -159,7 +227,7 @@ export default class CreateBaseBooking extends React.Component {
             type="checkbox"
             id={`attr-${index}-is-required`}
             className="input-checkbox"
-            value={attribute.is_required}
+            checked={attribute.is_required}
             onChange={onRequiredChange}
           />
           <label htmlFor={`attr-${index}-is-required`}>Required</label>
@@ -168,9 +236,75 @@ export default class CreateBaseBooking extends React.Component {
     );
   }
 
+  renderEntityRow(entity, index) {
+    const onSelectChange = event => {
+      const newEntity = { ...entity };
+
+      newEntity.selected = !!event.target.checked;
+
+      this.handleEntityChange(newEntity, index);
+    };
+
+    const onRequiredChange = event => {
+      const newEntity = { ...entity };
+
+      newEntity.is_required = !!event.target.checked;
+
+      this.handleEntityChange(newEntity, index);
+    };
+
+    const entityOption = getEntityTypeOption(entity.type);
+
+    return (
+      <div
+        className={classnames('entity', {
+          'entity--unselect': !entity.selected
+        })}
+        key={index}
+      >
+        <div className="form-control form-control--row-vertical-center">
+          <input
+            type="checkbox"
+            className="input-checkbox"
+            checked={entity.selected}
+            onChange={onSelectChange}
+          />
+        </div>
+
+        <div className="form-control">
+          <p>{entity.name}</p>
+        </div>
+
+        <div className="form-control">
+          <p>{entityOption.label}</p>
+        </div>
+
+        {entity.allowRequired ? (
+          <div className="form-control form-control--row-vertical-center">
+            <input
+              type="checkbox"
+              id={`entity-${index}-is-required`}
+              className="input-checkbox"
+              checked={entity.is_required}
+              onChange={onRequiredChange}
+              disabled={!entity.allowRequired}
+            />
+            <label htmlFor={`entity-${index}-is-required`}>Required</label>
+          </div>
+        ) : (
+          <div className="form-control form-control--row-vertical-center">
+            <p>N/A</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
   render() {
     const { baseEntityType } = this.props;
     const { baseEntityTypeList } = baseEntityType;
+
+    const { entities } = this.state;
 
     return (
       <div className="booking-base__create create">
@@ -220,11 +354,33 @@ export default class CreateBaseBooking extends React.Component {
                   getOptionLabel={option => option.name}
                   value={getBaseEntityTypeOption(
                     baseEntityTypeList,
-                    this.state.baseEntityType
+                    this.state.baseEntityTypeId
                   )}
                   onChange={this.onBaseEntityTypeChange}
                 />
               </div>
+
+              {entities && entities.length ? (
+                <div className="entity entity__header">
+                  <div className="form-control">&nbsp;</div>
+
+                  <div className="form-control">
+                    <h4>Field</h4>
+                  </div>
+
+                  <div className="form-control">
+                    <h4>Type</h4>
+                  </div>
+
+                  <div className="form-control">
+                    <h4>Required</h4>
+                  </div>
+                </div>
+              ) : null}
+
+              {entities && entities.length
+                ? entities.map(this.renderEntityRow)
+                : null}
             </div>
           </form>
         </div>
