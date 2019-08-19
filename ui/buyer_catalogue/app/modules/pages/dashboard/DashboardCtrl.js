90a34d32d458c20e05ef88070dedb7b7afa0b5fd
console42 <console42@wikasta.com>
2019-08-19 16:43:25 +0530

    date graph bars collapsing issue fixed

--- ui/buyer_catalogue/app/modules/pages/dashboard/DashboardCtrl.js
+++ ui/buyer_catalogue/app/modules/pages/dashboard/DashboardCtrl.js
@@ -2002,6 +2002,9 @@
           if(Object.keys(response.data.data.supplier_data).length > 4 ){
             $scope.stackedBarChartSocietyWise.chart['width'] = Object.keys(response.data.data.supplier_data).length * 100;
           }
+          if(Object.keys(response.data.data.date_data).length > 4 ){
+            $scope.stackedBarChartDateWise.chart['width'] = Object.keys(response.data.data.date_data).length * 100;
+          }
 
           $scope.campaignLeadsData = response.data.data;
           $scope.showPerfMetrics = $scope.perfMetrics.leads;
