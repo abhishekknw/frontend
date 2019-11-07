9b1ed20a7c4ba818cdc6c7b870e691c0a25282ad
Bhawana Verma <bhawanaverma@Bhawanas-MacBook-Pro.local>
2019-11-07 12:40:13 +0530

    Added back button in list suppliers page

--- machadalo/src/components/OperationsDashboard/SupplierAnalytics/SupplierList.jsx
+++ machadalo/src/components/OperationsDashboard/SupplierAnalytics/SupplierList.jsx
@@ -6,12 +6,22 @@ class SupplierList extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     const { suppliers, campaign_id } = this.props.location.state;
     let { status } = this.props.location.state;
     status = status.charAt(0).toUpperCase() + status.slice(1);
     return (
       <div>
+        <button
+          type="button"
+          className="btn btn--danger"
+          onClick={() => this.props.history.push(`/r/operations-dashboard`)}
+          style={{ marginTop: '10px' }}
+        >
+          <i className="fa fa-arrow-left" aria-hidden="true" />
+          &nbsp; Back
+        </button>
         {suppliers.length > 0 && (
           <InnerGrid
             columns={getSupplierColumn()}
