439689a568fcd49628fc568695269c6390a75708
bhaskardwi <bhaskar.dwivedi@synergytop.com>
2020-02-07 16:24:21 +0530

    1.freeze for meeting converted (It is working for meeting fixed) is changed  2.Contact Details-  readable remove button hide (when status is completed)  3.Mode Of Payment- In details popup- Edit button hide (when status is completed)  4.Receipt- fixed issue show option for add image and comment

--- ui/buyer_catalogue/app/modules/pages/releaseCampaignPlan/releaseCampaign.controller.js
+++ ui/buyer_catalogue/app/modules/pages/releaseCampaignPlan/releaseCampaign.controller.js
@@ -1554,7 +1554,7 @@ angular.module('catalogueApp')
 
         $scope.completionStatus = function (idOfSupplier) {
           let indexOfSupplier = $scope.releaseDetails.shortlisted_suppliers.findIndex(x => x.id === idOfSupplier);
-          if ($scope.releaseDetails.shortlisted_suppliers[indexOfSupplier].booking_status !== "BK" && $scope.releaseDetails.shortlisted_suppliers[indexOfSupplier].booking_status !== "MF") {
+          if ($scope.releaseDetails.shortlisted_suppliers[indexOfSupplier].booking_status !== "BK" && $scope.releaseDetails.shortlisted_suppliers[indexOfSupplier].booking_status !== "MC") {
             swal(constants.name, constants.booking_completion_status, constants.warning);
             $scope.releaseDetails.shortlisted_suppliers[indexOfSupplier].is_completed = false;
           }
