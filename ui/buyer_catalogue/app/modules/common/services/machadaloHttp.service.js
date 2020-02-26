38b88fad4cf9c7ba254d024da6a5a3c61c339eee
Shailesh <shailesh@mit.tc>
2020-02-26 11:45:20 +0530

    added expiry globaly in request

--- ui/buyer_catalogue/app/modules/common/services/machadaloHttp.service.js
+++ ui/buyer_catalogue/app/modules/common/services/machadaloHttp.service.js
@@ -1,5 +1,5 @@
 angular.module('machadaloCommon')
-.factory('machadaloHttp', ['$http', '$rootScope', function($http, $rootScope) {
+.factory('machadaloHttp', ['$http', '$rootScope', '$location', function($http, $rootScope, $location) {
    var machadaloHttp = {};
    machadaloHttp.baseURL = APIBaseUrl;
 
@@ -7,11 +7,23 @@ angular.module('machadaloCommon')
       config.headers = config.headers || {};
       config.headers['Authorization'] = 'JWT ' + $rootScope.globals.currentUser.token;
    }
-
+   var stopLoop = true;
    angular.forEach(['get', 'delete', 'head', 'jsonp'], function (name) {
       machadaloHttp[name] = function(url, config) {
          config = config || {};
          extendHeaders(config);
+         let promise = $http[name](machadaloHttp.baseURL + url, config);
+        promise.then(function (response) {
+        }, function (reason) {
+          if(stopLoop) {
+           if(reason.status == 403){
+              alert("Session has expired, please login again");
+              $location.path('/login');
+              localStorage.clear();
+              stopLoop = false;
+           }
+         }
+        });
          return $http[name](machadaloHttp.baseURL + url, config);
       };
    });
@@ -20,7 +32,19 @@ angular.module('machadaloCommon')
       machadaloHttp[name] = function(url, data, config) {
          config = config || {};
          extendHeaders(config);
-         return $http[name](machadaloHttp.baseURL + url, data, config);
+         let promise = $http[name](machadaloHttp.baseURL + url, data, config);
+          promise.then(function (response) {
+          }, function (reason) {
+            if(stopLoop) {
+             if(reason.status == 403){
+                alert("Session has expired, please login again");
+                $location.path('/login');
+                localStorage.clear();
+                stopLoop = false;
+             }
+           }
+          });
+          return $http[name](machadaloHttp.baseURL + url, data, config);
       };
    });
 
