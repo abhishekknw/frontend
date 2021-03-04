f47eb4e4d5da5b85700bcb8c8d5793ec189ba3e9
Jitendrapatel <jitendra.patel@synergytop.com>
2021-03-04 13:03:25 +0530

    removed console

--- ui/buyer_catalogue/app/modules/pages/b2bdashboard/B2BDashboardCtrl.js
+++ ui/buyer_catalogue/app/modules/pages/b2bdashboard/B2BDashboardCtrl.js
@@ -560,7 +560,6 @@
     }
 
     $scope.getPurchasedNotPurchasedLead = function (CampaignId, campaignName, leadStatus) {
-      console.log('11111111111111111',$scope.filterType, $scope.selectedSupplierType.code);
       $scope.leadPurchasedStatus = leadStatus;
       $scope.CampaignNameofLeads = campaignName;
       B2BDashboardService.purchasedNotPurchasedLead(CampaignId, $scope.filterType,$scope.selectedSupplierType.code)
