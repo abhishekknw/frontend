219c9a059e483e293ea49befd56b878280b15897
Jitendrapatel <jitendra.patel@synergytop.com>
2021-08-06 10:59:28 +0530

    created aisensy module

---
+++ ui/buyer_catalogue/app/modules/pages/covid/aisensy/aisensy.controller.js
@@ -0,0 +1,19 @@
+angular.module('machadaloPages').filter('replace', [function () {
+    return function (input, from, to) {
+
+        if (input === undefined) {
+            return;
+        }
+        var regex = new RegExp(from, 'g');
+        return input.replace(regex, to);
+    };
+}]).controller('aisensyCtrl',
+    ['$scope', '$rootScope', '$window', '$location', 'AuthService', 'suspenseLeadService', '$state', 'userService', 'constants', 'AuthService', 'vcRecaptchaService',
+        function ($scope, $rootScope, $window, $location, AuthService, suspenseLeadService, $state, userService, constants, AuthService, vcRecaptchaService) {
+            AuthService.Clear();
+
+     
+            
+
+        }]);
+
