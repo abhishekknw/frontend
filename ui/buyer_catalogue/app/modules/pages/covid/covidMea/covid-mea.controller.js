7e595e05576cac7c1b4ea30a9e9c4c624072121a
Divya Mogaveera <dsm14499@gmail.com>
2021-08-21 12:03:42 +0530

    API for caraosel videos

--- ui/buyer_catalogue/app/modules/pages/covid/covidMea/covid-mea.controller.js
+++ ui/buyer_catalogue/app/modules/pages/covid/covidMea/covid-mea.controller.js
@@ -11,53 +11,47 @@ angular.module('machadaloPages').filter('replace', [function () {
     ['$scope', '$rootScope', '$window', '$location', 'AuthService', 'suspenseLeadService', '$state', 'userService', 'constants', 'AuthService', 'vcRecaptchaService',
         function ($scope, $rootScope, $window, $location, AuthService, suspenseLeadService, $state, userService, constants, AuthService, vcRecaptchaService) {
             AuthService.Clear();
-
-     
-          
-
-
-
             var url = $location.url().split("/");
             // $scope.categorys = ['Hospital Beds', 'Cylinders','Refills', 'Concentrators'];
             let cat = url[1].substring(0, 1).toUpperCase() + url[1].substring(1);
             $scope.selectedCategory = cat;
-            if($scope.selectedCategory =='Doctors'){
+            if ($scope.selectedCategory == 'Doctors') {
                 $scope.selectedCategory = 'Free Online Doctor Consulation';
             }
-            console.log('++++++++++++++++++++++++++++',$scope.selectedCategory);
-            if($scope.selectedCategory =='Vaccinecenters'){
+            console.log('++++++++++++++++++++++++++++', $scope.selectedCategory);
+            if ($scope.selectedCategory == 'Vaccinecenters') {
                 $scope.selectedCategory = 'Vaccine Centers';
             }
-            if($scope.selectedCategory =='Ambulance'){
-                $scope.newSelectedCategory = 'Ambulance';  
-            } else if($scope.selectedCategory =='Plasma'){
-                $scope.newSelectedCategory = 'Plasma'; 
+            if ($scope.selectedCategory == 'Ambulance') {
+                $scope.newSelectedCategory = 'Ambulance';
+            } else if ($scope.selectedCategory == 'Plasma') {
+                $scope.newSelectedCategory = 'Plasma';
             } else {
                 $scope.newSelectedCategory = $scope.selectedCategory.slice(0, -1);
             }
 
             $scope.loading = true;
 
-            $scope.getCategory = function () {
+            $scope.getCategory1 = function () {
                 AuthService.getAllCategory()
                     .then(function onSuccess(response) {
                         $scope.categorysArray = response.data.data;
                         $scope.categorysArray.push(
                             {
-                            "category_code": "",
-                            "keyword": "MDCovidcases",
-                            "name": "Covid Cases",
-                        },
-                        {
-                            "category_code": "",
-                            "keyword": "MDVaccineCenters",
-                            "name": "Vaccine Centers",
-                        },
-                        {
-                            "category_code": "",
-                            "keyword": "MDConsulation",
-                            "name": "Free Online Doctor Consulation",
-                        });
+                                "category_code": "",
+                                "keyword": "MDCovidcases",
+                                "name": "Covid Cases",
+                            },
+                            {
+                                "category_code": "",
+                                "keyword": "MDVaccineCenters",
+                                "name": "Vaccine Centers",
+                            },
+                            {
+                                "category_code": "",
+                                "keyword": "MDConsulation",
+                                "name": "Free Online Doctor Consulation",
+                            });
                         $scope.categorysArrayNew = $scope.categorysArray;
                         for (let j in $scope.categorysArrayNew) {
                             if ($scope.categorysArrayNew[j].name == 'PuffCans') {
@@ -189,22 +183,22 @@ angular.module('machadaloPages').filter('replace', [function () {
                             // }
 
                             for (let k in $scope.volunteerData) {
-                                if($scope.volunteerData.length == 1){
+                                if ($scope.volunteerData.length == 1) {
                                     $scope.cylinderDetailData[j].Volunteer_Name = $scope.volunteerData[k].Volunteer_Name;
                                     $scope.cylinderDetailData[j].BitLink = $scope.volunteerData[k].BitLink;
                                 } else {
-                                if (!$scope.lastIndex || $scope.lastIndex == k) {
-                                    $scope.cylinderDetailData[j].Volunteer_Name = $scope.volunteerData[k].Volunteer_Name;
-                                    $scope.cylinderDetailData[j].BitLink = $scope.volunteerData[k].BitLink;
-                                    $scope.lastIndex = k;
+                                    if (!$scope.lastIndex || $scope.lastIndex == k) {
+                                        $scope.cylinderDetailData[j].Volunteer_Name = $scope.volunteerData[k].Volunteer_Name;
+                                        $scope.cylinderDetailData[j].BitLink = $scope.volunteerData[k].BitLink;
+                                        $scope.lastIndex = k;
+                                    }
+                                    if ($scope.volunteerData.length - 1 == k) {
+                                        $scope.lastIndex = JSON.parse($scope.lastIndex) + 1;
+                                    }
+                                    if ($scope.lastIndex == $scope.volunteerData.length) {
+                                        $scope.lastIndex = undefined;
+                                    }
                                 }
-                                if ($scope.volunteerData.length - 1 == k) {
-                                    $scope.lastIndex = JSON.parse($scope.lastIndex) + 1;
-                                }
-                                if ($scope.lastIndex == $scope.volunteerData.length) {
-                                    $scope.lastIndex = undefined;
-                                }
-                            }
                             }
                         }
                     }).catch(function onError(response) {
@@ -230,16 +224,16 @@ angular.module('machadaloPages').filter('replace', [function () {
                     param.feedback = '-Resources Available';
                     $scope.cylinderDetailData[index].resourcesAvailableButton = false;
                     swal("Feedback Removed", "Successfully", "success");
-                   
+
                 } else {
                     $scope.cylinderDetailData[index].resourcesAvailableButton = true;
                     swal("Feedback Accepted", "Successfully", "success");
                 }
                 AuthService.feedback(param)
-                .then(function onSuccess(response) {
-                }).catch(function onError(response) {
-                    console.log(response);
-                })
+                    .then(function onSuccess(response) {
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
 
             }
 
@@ -261,10 +255,10 @@ angular.module('machadaloPages').filter('replace', [function () {
                     swal("Feedback Accepted", "Successfully", "success");
                 }
                 AuthService.feedback(param)
-                .then(function onSuccess(response) {
-                }).catch(function onError(response) {
-                    console.log(response);
-                })
+                    .then(function onSuccess(response) {
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
             }
 
             $scope.wrongNumber = function (index) {
@@ -285,6 +279,140 @@ angular.module('machadaloPages').filter('replace', [function () {
                     swal("Feedback Accepted", "Successfully", "success");
                 }
             }
+
+            /*new mea controller code** */
+
+            $scope.getToc = function () {
+           
+                let param = {
+                    type_of_customer: "", 
+                    category: "", 
+                    subcategory: "",
+                    subsubcategory: ""
+                }
+                AuthService.getAllMeaData(param)
+                
+                    .then(function onSuccess(response) {
+                        console.log(response)
+                        $scope.typeofCustomer=response.data.data.Toc ; 
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+
+            $scope.getCategory= function (value) {
+                console.log(value)
+$scope.selectedToc=value;
+                console.log($scope.selectedToc)
+
+             
+                let param = {
+                    type_of_customer: $scope.selectedToc, 
+                    category: "", 
+                    subcategory: "",
+                    subsubcategory: ""
+                }
+                AuthService.getAllMeaData(param)
+                
+                    .then(function onSuccess(response) {
+                        console.log(response)
+                        $scope.categoryArray=response.data.data.category ; 
+                        console.log('1111111111111',$scope.categoryArray)
+
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+
+            $scope.getSubCategory= function (value) {
+                console.log(value)
+                $scope.selectedcategory=value;
+                console.log($scope.selectedcategory)
+               
+
+                console.log($scope.selectedcategory)
+
+
+                let param = {
+                    type_of_customer: $scope.selectedToc, 
+                    category: $scope.selectedcategory, 
+                    subcategory: "",
+                    subsubcategory: ""
+                }
+                AuthService.getAllMeaData(param)
+                
+                    .then(function onSuccess(response) {
+                        console.log(response)
+                        $scope.subcategoryArray=response.data.data.subcategory ; 
+                        console.log('1111111111111',$scope.subcategoryArray)
+
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+
+            $scope.getSubSubCategory= function (value) {
+                console.log(value)
+                $scope.selectedsubcategory=value;
+                console.log($scope.selectedsubcategory)
+
+                console.log($scope.selectedsubcategory)
+
+             
+                let param = {
+                    type_of_customer: $scope.selectedToc, 
+                    category: $scope.selectedcategory, 
+                    subcategory: $scope.selectedsubcategory,
+                    subsubcategory: ""
+                }
+                AuthService.getAllMeaData(param)
+                
+                    .then(function onSuccess(response) {
+                        console.log(response)
+                        $scope.subsubcategoryArray=response.data.data.subsubcategory ; 
+                        $scope.contentArray=response.data.data.content ; 
+                        $scope.contentArray['compairing numbers'].push({ Comment: "Adding two digits on a number line"});
+                        $scope.contentArray['compairing numbers'].push({ Comment: "Adding1 two digits on a number line"});
+                        $scope.contentArray['compairing numbers'].push({ Comment: "Adding22 two digits on a number line"});
+                        $scope.contentArray['compairing numbers'].push({ Comment: "Adding333 two digits on a number line"});
+                        console.log('1111111111111',$scope.subsubcategoryArray)
+
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+
+
+            
+
+            $scope.getHeaderCategory = function(value){
+                console.log($scope.selectedTocHeader)
+                console.log(value)
+                $scope.selectedTocHeader=value
+            }
+
+            $scope.getHeaderSubCategory = function(value){
+                console.log($scope.selectedcategoryHeader)
+                console.log(value)
+                $scope.selectedcategoryHeader=value
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }]);
 
