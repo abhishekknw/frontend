0a92b443fb825fc67b4d8dd3293f92bc3e5a3a4c
Jitendrapatel <jitendra.patel@synergytop.com>
2021-08-25 15:39:24 +0530

    changed in aisensy filter

--- ui/buyer_catalogue/app/modules/pages/covid/aisensy/aisensy.controller.js
+++ ui/buyer_catalogue/app/modules/pages/covid/aisensy/aisensy.controller.js
@@ -1,12 +1,9 @@
-angular.module('machadaloPages').filter('replace', [function () {
-    return function (input, from, to) {
-
-        if (input === undefined) {
-            return;
+angular.module('machadaloPages').filter('firstlater', [function () {
+    return function(input, scope) {
+        if (input != null)
+        input = input.charAt(0).toUpperCase();
+        return input;
         }
-        var regex = new RegExp(from, 'g');
-        return input.replace(regex, to);
-    };
 }]).controller('aisensyCtrl',
     ['$scope', '$rootScope', '$window', '$location', 'AuthService', 'suspenseLeadService', '$state', 'userService', 'constants', 'AuthService', 'vcRecaptchaService',
         function ($scope, $rootScope, $window, $location, AuthService, suspenseLeadService, $state, userService, constants, AuthService, vcRecaptchaService) {
@@ -27,7 +24,7 @@ let hoverEnter = index =>{
 
 // AIsensy controller
 $scope.getActiveUser = function () {
-           alert("API call");
+        
     AuthService.getAllActiveUserData()
     
         .then(function onSuccess(response) {
@@ -41,7 +38,7 @@ $scope.getActiveUser = function () {
 }
 
 $scope.userDetail = function (value) {
-    alert("Phone Number");
+   
     let param = {
         phoneNumber:value,
         start:0,
@@ -69,7 +66,7 @@ $scope.userDetail = function (value) {
 }
 
 $scope.userChat = function (value) {
-    alert("Chat");
+
     let param = {
         phoneNumber:value,
         start:value,
