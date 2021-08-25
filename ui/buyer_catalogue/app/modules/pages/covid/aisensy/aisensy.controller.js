4e63ec4c41cf61e46b93dd100b97a101d39bbcde
Jitendrapatel <jitendra.patel@synergytop.com>
2021-08-25 20:12:38 +0530

    changed in chat html

--- ui/buyer_catalogue/app/modules/pages/covid/aisensy/aisensy.controller.js
+++ ui/buyer_catalogue/app/modules/pages/covid/aisensy/aisensy.controller.js
@@ -1,88 +1,91 @@
 angular.module('machadaloPages').filter('firstlater', [function () {
-    return function(input, scope) {
+    return function (input, scope) {
         if (input != null)
-        input = input.charAt(0).toUpperCase();
+            input = input.charAt(0).toUpperCase();
         return input;
-        }
+    }
 }]).controller('aisensyCtrl',
     ['$scope', '$rootScope', '$window', '$location', 'AuthService', 'suspenseLeadService', '$state', 'userService', 'constants', 'AuthService', 'vcRecaptchaService',
         function ($scope, $rootScope, $window, $location, AuthService, suspenseLeadService, $state, userService, constants, AuthService, vcRecaptchaService) {
             AuthService.Clear();
 
-     
+
             let gooIndex = document.getElementById('goo-index');
-let hoverEnter = index =>{
-  gooIndex.style.top = 100*index+'px';
-  let allScreens = document.querySelectorAll('.screen');
-  allScreens.forEach(e=>{
-    e.classList.remove('visible')
-  })
-  let nowVisible = document.getElementById('screen_'+index);
-  nowVisible.classList.add('visible');
-}
+            let hoverEnter = index => {
+                gooIndex.style.top = 100 * index + 'px';
+                let allScreens = document.querySelectorAll('.screen');
+                allScreens.forEach(e => {
+                    e.classList.remove('visible')
+                })
+                let nowVisible = document.getElementById('screen_' + index);
+                nowVisible.classList.add('visible');
+            }
 
 
-// AIsensy controller
-$scope.getActiveUser = function () {
-        
-    AuthService.getAllActiveUserData()
-    
-        .then(function onSuccess(response) {
-            console.log(response)
-            
-            $scope.activeUserData=response.data.data ; 
-            console.log($scope.activeUserData)
-        }).catch(function onError(response) {
-            console.log(response);
-        })
-}
+            // AIsensy controller
+            $scope.getActiveUser = function () {
 
-$scope.userDetail = function (value) {
-   
-    let param = {
-        phoneNumber:value,
-        start:0,
-        end:10
-    }
-     AuthService.getAllUserDetailData(param)
+                AuthService.getAllActiveUserData()
 
- .then(function onSuccess(response) {
-     console.log(response)
-     
-     $scope.userDetailData=response.data.data; 
- }).catch(function onError(response) {
-     console.log(response);
- })
- AuthService.getAllUserChatData(param)
+                    .then(function onSuccess(response) {
+                        console.log(response)
 
- .then(function onSuccess(response) {
-     console.log(response)
-     
-     $scope.userChatData=response.data.data; 
-     console.log("1234", $scope.userChatData) 
- }).catch(function onError(response) {
-     console.log(response);
- })
-}
+                        $scope.activeUserData = response.data.data;
+                        console.log($scope.activeUserData)
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
 
-$scope.userChat = function (value) {
+            $scope.userDetail = function (value) {
+                $scope.showChatModule = true;
+                let param = {
+                    phoneNumber: value,
+                    start: 0,
+                    end: 10
+                }
+                AuthService.getAllUserDetailData(param)
 
-    let param = {
-        phoneNumber:value,
-        start:value,
-        end:value
-    }
-     AuthService.getAllUserChatData(param)
+                    .then(function onSuccess(response) {
+                        console.log(response)
 
- .then(function onSuccess(response) {
-     console.log(response)
-     
-     $scope.userChatData=response.data.data;
-     
- }).catch(function onError(response) {
-     console.log(response);
- })
-}
+                        $scope.userDetailData = response.data.data;
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+                AuthService.getAllUserChatData(param)
+
+                    .then(function onSuccess(response) {
+                        console.log(response)
+
+                        $scope.userChatData = response.data.data;
+                        console.log("1234", $scope.userChatData)
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+
+            $scope.userChat = function (value) {
+
+                let param = {
+                    phoneNumber: value,
+                    start: value,
+                    end: value
+                }
+                AuthService.getAllUserChatData(param)
+
+                    .then(function onSuccess(response) {
+                        console.log(response)
+
+                        $scope.userChatData = response.data.data;
+
+                    }).catch(function onError(response) {
+                        console.log(response);
+                    })
+            }
+            $scope.hideChatModule = function (value) {
+                $scope.showChatModule = false;
+            }
 
 
 
