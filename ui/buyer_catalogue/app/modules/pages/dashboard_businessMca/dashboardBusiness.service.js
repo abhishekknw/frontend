252de68b3e72a6acf6cd10b6d3c584d12b0b9215
synergytop <you@example.com>
2022-08-16 19:38:00 +0530

    API implemetation on suspenseLead Modal and business MCA dashboard

---
+++ ui/buyer_catalogue/app/modules/pages/dashboard_businessMca/dashboardBusiness.service.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+// angular.module('catalogueApp')
+//   .factory('businessService', ['machadaloHttp', '$stateParams', '$rootScope', '$routeParams', '$location', '$http',
+//     function (machadaloHttp, $stateParams, $scope, $rootScope, $routeParams, $location, $http) {
+//         businessService.leadDecisionPanding = function () {
+//             var url = "http://localhost:8000/v0/ui/b2b/get-dynamic-basic-lead-form-headers/?campaign_id=DULDUL258C&supplier_type=all&next_page=0";
+//             alert(1);
+//             return machadaloHttp.get(url);
+//         }
+//     return businessService;
+// }]);
