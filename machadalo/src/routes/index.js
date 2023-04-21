686fddc305c30a26893ee7f473ffb57f82e77d33
SynergyTop <suyashpandey.synergytop@gmail.com>
2023-04-21 10:16:44 +0530

    conditional routes for supplier agency

--- machadalo/src/routes/index.js
+++ machadalo/src/routes/index.js
@@ -7,11 +7,17 @@ import {
 } from 'react-router-dom';
 
 import LayoutContainer from './../containers/LayoutContainer';
+import SupplierRoutes from '../Dashboards/SupplierAgency/routes';
 
 export default function Routes() {
+  let userInfo = JSON.parse(localStorage.getItem('userInfo'))
   return (
     <Router>
       <Switch>
+        {
+          userInfo.profile.organisation.category === "SUPPLIER_AGENCY" &&
+          <Route path='/r/supplier' component={SupplierRoutes} />
+        }
         <Route path="/r" component={LayoutContainer} />
         <Route path="*">
           <Redirect to="/r/404" />
