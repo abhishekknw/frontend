9f6f8d6f15b782e93296c1257a05b3e8b9603d50
usersuyash <suyashpandey.synergytop@gmail.com>
2023-04-25 12:00:10 +0530

    new client dashboard

--- machadalo/src/Dashboards/ClientDashboard/ClientDashboard.jsx
+++ machadalo/src/Dashboards/ClientDashboard/ClientDashboard.jsx
@@ -1,10 +1,63 @@
 import React, { useState } from 'react';
 import ClientHeader from '../../Dashboards/headers/Header';
 import './index.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const ClientDashboard = (props) => {
   return (<>
-  <ClientHeader />
-  <h4>New Client Dashboard</h4>
+    <ClientHeader />
+    <div className='pt-4'>
+      <Container>
+        <h4>WELCOME BACK, KATHERINE</h4>
+      </Container>
+    </div>
+    <div>
+      <Container>
+        <Row className='pt-5'>
+          <Col sm={3}>
+            <div>
+            <div>
+              <span>image</span>
+            </div>
+            <div>
+              <span className='count-num'>32,600</span><br />
+              <span className='count-value'>total downloads</span>
+            </div>
+            </div>
+          </Col>
+          <Col sm={3}>
+            <div>
+              <div>
+                <span>image</span>
+              </div>
+              <div>
+                <span className='count-num'>32,600</span><br />
+                <span className='count-value'>total downloads</span>
+              </div>
+            </div>
+          </Col>
+          <Col sm={3}>
+            <div>  <div>
+              <span>image</span>
+            </div>
+            <div>
+              <span className='count-num'>32,600</span><br />
+              <span className='count-value'>total downloads</span>
+            </div>     </div>
+          </Col>
+          <Col sm={3}>
+            <div>  <div>
+              <span>image</span>
+            </div>
+            <div>
+              <span className='count-num'>32,600</span><br />
+              <span className='count-value'>total downloads</span>
+            </div>     </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </>
   );
 };
