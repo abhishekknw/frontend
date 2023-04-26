64ed42656c47e421e6315fb136ee11ef4895335e
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-04-26 19:47:48 +0530

    intsall matrial react table and react-charjs

--- machadalo/src/Dashboards/ClientDashboard/Graph.jsx
+++ machadalo/src/Dashboards/ClientDashboard/Graph.jsx
@@ -1,82 +1,56 @@
 import React from 'react';
-import ApexCharts from 'apexcharts'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [2000,3458,8645,9574,3948,4949,2230],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [2000,3458,8645,4574,3748,4949,2230],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 export default function GraphChart() {
-  var options = {
-    chart: {
-      type: 'bar'
-    },
-    series: [
-      {
-        name: 'sales',
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-      }
-    ],
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-    }
-  }
-  
-  var chart = new ApexCharts(document.querySelector('#chart'),options)
-  console.log(chart,"chart")
-  chart.render()
-//     let GraphData= {
-          
-//         series: [{
-//           name: 'Net Profit',
-//           data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-//         }, {
-//           name: 'Revenue',
-//           data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-//         }, {
-//           name: 'Free Cash Flow',
-//           data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-//         }],
-//         options: {
-//           chart: {
-//             type: 'bar',
-//             height: 350
-//           },
-//           plotOptions: {
-//             bar: {
-//               horizontal: false,
-//               columnWidth: '55%',
-//               endingShape: 'rounded'
-//             },
-//           },
-//           dataLabels: {
-//             enabled: false
-//           },
-//           stroke: {
-//             show: true,
-//             width: 2,
-//             colors: ['transparent']
-//           },
-//           xaxis: {
-//             categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-//           },
-//           yaxis: {
-//             title: {
-//               text: '$ (thousands)'
-//             }
-//           },
-//           fill: {
-//             opacity: 1
-//           },
-//           tooltip: {
-//             y: {
-//               formatter: function (val) {
-//                 return "$ " + val + " thousands"
-//               }
-//             }
-//           }
-//         },
-//       };
-// var chart = new ApexCharts(document.querySelector("#chart"), GraphData.options, GraphData.series);
-// chart.render();
-    // return(
-    //     <div id="chart">
-    //         <ApexCharts options={GraphData.options} series={GraphData.series} type="bar" height={350} />
-    //     </div>
-    // )
+  return <Bar options={options} data={data} />;
 }
---
+++ machadalo/src/Dashboards/ClientDashboard/Tables/UserWiseList.jsx
@@ -0,0 +1,128 @@
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { Button } from 'react-bootstrap';
+//nested data is ok, see accessorKeys in ColumnDef below
+const data = [
+  {
+    key:"1",
+    address: '261 Erdman Ford',
+    city: 'Indore',
+    Total_Leads_count:"5000",
+    Current_week_count :"1000",
+    Last_week_count:"300",
+    Current_month_count:"3000",
+    This_quarter_count:"700",
+    Total_comment_updated_count:"898",
+    Total_status_updated_count:"775"
+  },
+  {
+    key:"2",
+    address: '261 Erdman Ford',
+    city: 'Indore',
+    Total_Leads_count:"5000",
+    Current_week_count :"1000",
+    Last_week_count:"300",
+    Current_month_count:"3000",
+    This_quarter_count:"700",
+    Total_comment_updated_count:"898",
+    Total_status_updated_count:"775"
+  }, {
+    key:"3",
+    address: '261 Erdman Ford',
+    city: 'Indore',
+    Total_Leads_count:"5000",
+    Current_week_count :"1000",
+    Last_week_count:"300",
+    Current_month_count:"3000",
+    This_quarter_count:"700",
+    Total_comment_updated_count:"898",
+    Total_status_updated_count:"775"
+  }, {
+    key:"4",
+    address: '261 Erdman Ford',
+    city: 'Indore',
+    Total_Leads_count:"5000",
+    Current_week_count :"1000",
+    Last_week_count:"300",
+    Current_month_count:"3000",
+    This_quarter_count:"700",
+    Total_comment_updated_count:"898",
+    Total_status_updated_count:"775"
+  }, {
+    key:"5",
+    address: '261 Erdman Ford',
+    city: 'Indore',
+    Total_Leads_count:"5000",
+    Current_week_count :"1000",
+    Last_week_count:"300",
+    Current_month_count:"3000",
+    This_quarter_count:"700",
+    Total_comment_updated_count:"898",
+    Total_status_updated_count:"775"
+  },
+];
+
+const UserWiseList = () => {
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'key', //access nested data with dot notation
+        header: 'S.No.',
+      },
+      {
+        accessorKey: 'city',
+        header: 'City Name',
+      },
+      {
+        accessorKey: 'Total_Leads_count', //normal accessorKey
+        header: 'Total Leads count',
+      },
+      {
+        accessorKey: 'Current_week_count',
+        header: 'Current week count',
+      },
+      {
+        accessorKey: 'Last_week_count',
+        header: 'Last week count',
+      },
+      {
+        accessorKey: 'Current_month_count',
+        header: 'Current month count',
+      },
+      {
+        accessorKey: 'This_quarter_count',
+        header: 'This quarter count',
+      },
+      {
+        accessorKey: 'Total_comment_updated_count',
+        header: 'Total comment updated count',
+      },
+      {
+        accessorKey: 'Total_status_updated_count',
+        header: 'Total status updated count',
+      },
+      {
+        // accessorKey: 'Total_status_updated_count',
+        header: 'View',
+        accessorFn: rowData => <Button>View</Button>
+      },
+      {
+        accessorFn: rowData => <Button>Download</Button>,
+        header: 'Download',
+      },
+      {
+        header: 'Email',
+        accessorFn: rowData => <Button>Email</Button>,
+      },
+    ],
+    [],
+  );
+
+  return <MaterialReactTable 
+  columns={columns} 
+  data={data} 
+  />;
+};
+
+export default UserWiseList;
