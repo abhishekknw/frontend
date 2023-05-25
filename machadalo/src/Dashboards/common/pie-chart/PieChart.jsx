ba5c06f925b644ec7076507b0ff5d7b5eff5b42e
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-05-25 17:09:38 +0530

    import pie chart

--- machadalo/src/Dashboards/common/pie-chart/PieChart.jsx
+++ machadalo/src/Dashboards/common/pie-chart/PieChart.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
 export default function PieChart() {
+  const data = {
+    labels: ['B2B','B2C'],
+    datasets: [
+      {
+        label: 'Leads',
+        data: [12, 2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <>
-    <h2>Pie Chart</h2>
+    <Pie data={data} />
     </>
   );
 }
