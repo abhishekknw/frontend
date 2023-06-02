c0b7ebaa96b6e33cb7f41b461bfd0a9796474917
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-06-02 16:28:17 +0530

    remove caledr from machadalo.jsx file

--- machadalo/src/Dashboards/MachadaloDashboard/machadaloDashboard.jsx
+++ machadalo/src/Dashboards/MachadaloDashboard/machadaloDashboard.jsx
@@ -4,7 +4,6 @@ import DateFilter from '../common/date-range-filter/dateFilter';
 import GraphChart from '../common/Graphs/Graph';
 import LeadsTable from './Leads';
 import PieChart from '../common/pie-chart/PieChart';
-import Calendar from '../common/Calendar/Calendar';
 
 export default function MachadaloDashboard(props) {
 
@@ -16,7 +15,6 @@ export default function MachadaloDashboard(props) {
       <div className="container">
         <div className="machadalo-deshboard">
           <MachadaloHeader />
-          {/* <Calendar /> */}
           <DateFilter onDateChange={getDates} />
           <div className="dx">
             <div className="graph-chart">
