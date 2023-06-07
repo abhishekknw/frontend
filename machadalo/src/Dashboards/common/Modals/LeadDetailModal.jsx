b2e1b78d41320b7a3bc557901048fce39033309b
usersuyash <suyashpandey.synergytop@gmail.com>
2023-06-07 10:56:41 +0530

    modal mofification

--- machadalo/src/Dashboards/common/Modals/LeadDetailModal.jsx
+++ machadalo/src/Dashboards/common/Modals/LeadDetailModal.jsx
@@ -19,7 +19,7 @@ function LeadDetailModal() {
           Launch demo modal
         </Button>
       </div> */}
-      <Modal show={showHideModal.leadDetail.show} onHide={handleClose} className='wpModal'>
+      <Modal show={showHideModal.leadDetail.show} onHide={handleClose} className='wpModal lead-modal'>
         <Modal.Header closeButton>
           <Modal.Title>Lead Detail</Modal.Title>
 
@@ -30,28 +30,80 @@ function LeadDetailModal() {
             <div className='col-md-4'><p>Assigned by: </p></div>
             <div className='col-md-4'><p>Assigned to:</p></div>
           </div>
-          <div>
-            <p className='strip-div'> Sector:</p>
-            <p>Test Sector</p>
+          <div className='row'>
+            <div className='col-md-6'>
+
+              <div>
+                <p className='strip-div'> Sector:</p>
+                <p>Test Sector</p>
+              </div>
+              <div>
+                <p className='strip-div'> Sector Specific Questions :</p>
+                <p className='d-flex justify-content-between'><span>Q1&nbsp;. &nbsp;Response :</span><span>?</span></p>
+                <p className='d-flex justify-content-between'><span>Q2&nbsp;. &nbsp;Response :</span><span>?</span></p>
+                <p className='d-flex justify-content-between'><span>Q3&nbsp;. &nbsp;Response :</span><span>?</span></p>
+              </div>
+              <div>
+                <p className='strip-div'>Address :</p>
+                <p>test adddress Prayagraj, Uttar Pradesh 211011,Prayagraj, Allahabad,Uttar Pradesh,India, 110002,(18.222,72.0)</p>
+              </div>
+
+
+            </div>
+            <div className='col-md-6'>
+
+              <div>
+                <p className='strip-div'>User Details:</p>
+                <p>aman, 9752276168 , Secretary</p>
+              </div>
+
+              <div className='status-div'>
+                <div className='d-flex align-items-center strip-div justify-content-between'>
+                  <p className='p-0'>Comments :</p>
+
+                  <div className="form-group email-dropdown">
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        All
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.Item href="#">All</Dropdown.Item>
+                        <Dropdown.Item href="#">Machadalo</Dropdown.Item>
+                        <Dropdown.Item href="#">Client</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </div>
+                <div className='comment-box-outer'>
+                  <div className='comment-box'>
+                    <p >Hello</p>
+                    <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
+                  </div>
+                  <div className='comment-box'>
+                    <p >Hello</p>
+                    <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
+                  </div>
+                  <div className='comment-box'>
+                    <p >Hello</p>
+                    <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
+                  </div>
+                </div>
+                <textarea rows={2} className='fullwidth' ></textarea>
+                <Button variant="secondary" className='btn btn-primary'>
+                  Add
+                </Button>
+              </div>
+            </div>
           </div>
-          <div>
-            <p className='strip-div'> Sector Specific Questions :</p>
-            <p className='d-flex justify-content-between'><span>Q1&nbsp;. &nbsp;Response :</span><span>?</span></p>
-            <p className='d-flex justify-content-between'><span>Q2&nbsp;. &nbsp;Response :</span><span>?</span></p>
-            <p className='d-flex justify-content-between'><span>Q3&nbsp;. &nbsp;Response :</span><span>?</span></p>
-          </div>
-          <div>
-            <p className='strip-div'>User Details:</p>
-            <p>aman, 9752276168 , Secretary</p>
-          </div>
-          <div>
-            <p className='strip-div'>Address :</p>
-            <p>testadddress Prayagraj, Uttar Pradesh 211011,Prayagraj, Allahabad,Uttar Pradesh,India, 110002,(18.222,72.0)</p>
-          </div>
+
+
+
+
           <div className='status-div'>
-            <p className='strip-div'>Status History :</p> 
+            <p className='strip-div'>Status History :</p>
             <div>
-            <div className="table-responsive">
+              <div className="table-responsive">
                 <table className="table">
                   <tr>
                     <td>
@@ -61,20 +113,20 @@ function LeadDetailModal() {
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
                           </div>
-                         
-                          
+
+
                         </div>
                         <div className="text_circle done">
                           <div className="circle">
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
@@ -86,7 +138,7 @@ function LeadDetailModal() {
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
@@ -97,7 +149,7 @@ function LeadDetailModal() {
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
@@ -108,7 +160,7 @@ function LeadDetailModal() {
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
@@ -119,7 +171,7 @@ function LeadDetailModal() {
                             <h4>Leads Verified By Machadalo</h4>
                           </div>
                           <a className="tvar"><span data-toggle="popover" title="Sample text" data-trigger="hover"
-                              data-placement="top" data-content="Ukázkový text při hover"></span></a>
+                            data-placement="top" data-content="Ukázkový text při hover"></span></a>
                           <div className="subline">
                             <p>1/2/20223</p>
                             <p>Vidhidevlopment</p>
@@ -133,43 +185,7 @@ function LeadDetailModal() {
               </div>
             </div>
           </div>
-          <div className='status-div'>
-            <div className='d-flex align-items-center strip-div justify-content-between'>
-              <p className='p-0'>Comments :</p>
 
-              <div className="form-group email-dropdown">
-                <Dropdown>
-                  <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    All
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#">All</Dropdown.Item>
-                    <Dropdown.Item href="#">Machadalo</Dropdown.Item>
-                    <Dropdown.Item href="#">Client</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-            <div className='comment-box-outer'>
-              <div className='comment-box'>
-                <p >Hello</p>
-                <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
-              </div>
-              <div className='comment-box'>
-                <p >Hello</p>
-                <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
-              </div>
-              <div className='comment-box'>
-                <p >Hello</p>
-                <p><span><BsFillPersonFill /></span>: <span>June 6, 2023 </span><span>1:42:38 PM </span></p>
-              </div>
-            </div>
-            <textarea rows={2} className='fullwidth' ></textarea>
-            <Button variant="secondary" className='btn btn-primary'>
-            Add 
-          </Button>
-          </div>
 
         </Modal.Body>
         <Modal.Footer>
