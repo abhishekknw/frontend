200d10d6526380804a639d336169de40270ccb9e
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-06-28 13:35:36 +0530

    send sent date in payload when uplate client status

--- ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/templateDashboardCtrl.js
+++ ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/templateDashboardCtrl.js
@@ -6626,7 +6626,8 @@
       }
 
       $scope.updateCallStatus = function (status, row) {
-        row.call_status = status
+        row.call_status = status;
+        row.sent_date = $scope.user_view.sent_date;
         templateDashboardService.updateCallStatus(row)
           .then(function onSuccess(response) {
             swal("", response.data.data, constants.success);
@@ -6993,6 +6994,69 @@
         $scope.getTransactionalTemplateUserDetail($scope.user_view.template_id, $scope.user_view.sent_date, null, $scope.user_view.template_name, $scope.user_view.search, $scope.paramsForSorting);
       }
       $scope.templateDetail();
+      $scope.userDetail = function (value, page) {
+
+        $scope.showChatModule = true;
+
+        $scope.NewsupplierAddUpdateData = {};
+        $scope.newSupplierPocModel = [];
+        $scope.Supplier_id = "";
+        $scope.societyNameList = [];
+        $scope.newSelectedArea = [];
+        $scope.Areas = ""
+
+        let param = {
+          nextPage: 1,
+          phoneNumber: 7006501835,
+        }
+        if (page) {
+          param.nextPage = page;
+        } else {
+          $scope.totalCount = 0;
+        }
+
+        $scope.pageCount = param.nextPage;
+        $scope.disableNextPagebutton = false;
+        AuthService.getAllUserDetailData(param)
+
+          .then(function onSuccess(response) {
+            $scope.userDetailData = response.data.data;
+            $scope.userChatPayload = $scope.userDetailData.payload;
+            $scope.NewsupplierAddUpdateData.phone_number = $scope.userDetailData.phone_number;
+            $scope.getSupplierDataByNumber($scope.userDetailData.phone_number);
+          }).catch(function onError(response) {
+            console.log(response);
+          })
+        AuthService.getAllUserChatData(param)
+          .then(function onSuccess(response) {
+            $scope.userChatData = response.data.data;
+            $scope.totalCount = $scope.userChatData.total_count;
+            if ($scope.totalCount > 20) {
+              let count = $scope.totalCount / 20;
+              if ($scope.pageCount < count) {
+                $scope.disableNextPagebutton = true;
+              }
+            }
+
+            if ($scope.userChatData && $scope.userChatData.payload && $scope.userChatData.payload.length > 0) {
+              for (let i in $scope.userChatData.payload) {
+                if ($scope.userChatData.payload[i].content && $scope.userChatData.payload[i].content.url) {
+                  let typesArray = $scope.userChatData.payload[i].content.contentType.split("/");
+                  $scope.userChatData.payload[i].content.types = typesArray;
+                  if (typesArray[0] == 'image') {
+                    $scope.userChatData.payload[i].content.url = $scope.userChatData.payload[i].content.url + typesArray[1]
+                  } else {
+                    let emdUrl = $scope.userChatData.payload[i].content.url + typesArray[1]
+                    $scope.userChatData.payload[i].content.url = $sce.trustAsResourceUrl(emdUrl);
+                  }
+                }
+              }
+
+            }
+          }).catch(function onError(response) {
+            console.log(response);
+          })
+      }
       // Template Dashboard end
 
     })
