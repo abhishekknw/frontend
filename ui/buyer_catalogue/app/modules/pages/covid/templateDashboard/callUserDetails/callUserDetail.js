5dd4ed46fdedf267e192c9a0bb47a68ff718d983
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-07-17 15:16:24 +0530

    will worked on user detail modal

--- ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
+++ ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
@@ -17,5 +17,17 @@ angular
         templateUrl: "modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.html",
         link: function ($scope, element, attrs) {
 
-            console.log("2133")
-        }}})
+          // $scope.formDataCondition = {show:false};
+          console.log("2133")
+
+          $scope.example14model = []; 
+          $scope.example14data = [{ id: 6, label: "Dulux" },{ id: 8, label: "Berger" }, { id: 9, label: "Asian" },{ id: 1, label: "Test" },{id: 10, label: "Asian" },{ id: 9, label: "Testing Company" }]; 
+          // $scope.example14settings = { scrollableHeight: '200px', scrollable: true };
+          $scope.example14settings = { smartButtonMaxItems: 3, smartButtonTextConverter: function(itemText, originalItem) { if (itemText === 'Jhon') { return 'Jhonny!'; } return itemText; } };
+
+          $scope.getFormDataBySector = function(){
+            // $scope.formDataCondition = {show:true};
+          }
+        }
+      }
+    })
