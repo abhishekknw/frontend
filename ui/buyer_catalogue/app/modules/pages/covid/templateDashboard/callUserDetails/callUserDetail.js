db904ad769adc7e0f71c9f15cbdfa30848faccb5
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-07-17 17:50:01 +0530

    add basic functionality in form

--- ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
+++ ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
@@ -17,16 +17,21 @@ angular
         templateUrl: "modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.html",
         link: function ($scope, element, attrs) {
 
-          // $scope.formDataCondition = {show:false};
-          console.log("2133")
-
-          $scope.example14model = []; 
-          $scope.example14data = [{ id: 6, label: "Dulux" },{ id: 8, label: "Berger" }, { id: 9, label: "Asian" },{ id: 1, label: "Test" },{id: 10, label: "Asian" },{ id: 9, label: "Testing Company" }]; 
+          $scope.showHideObj = {table:false};
+          $scope.example14model = [];
+          $scope.example14data = [{ id: 6, label: "Dulux" }, { id: 8, label: "Berger" }, { id: 9, label: "Asian" }, { id: 1, label: "Test" }, { id: 10, label: "Asian" }, { id: 9, label: "Testing Company" }];
           // $scope.example14settings = { scrollableHeight: '200px', scrollable: true };
-          $scope.example14settings = { smartButtonMaxItems: 3, smartButtonTextConverter: function(itemText, originalItem) { if (itemText === 'Jhon') { return 'Jhonny!'; } return itemText; } };
+          $scope.example14settings = { smartButtonMaxItems: 3, smartButtonTextConverter: function (itemText, originalItem) { if (itemText === 'Jhon') { return 'Jhonny!'; } return itemText; } };
 
-          $scope.getFormDataBySector = function(){
-            // $scope.formDataCondition = {show:true};
+          $scope.getFormDataBySector = function () {
+            $scope.showHideObj = {table:true,form:false};
+          }
+
+          $scope.showLeadDetails = function (){
+            $scope.showHideObj = {table:false,form:true};
+          }
+          $scope.backToTable = function () {
+            $scope.showHideObj = {table:true,form:false};
           }
         }
       }
