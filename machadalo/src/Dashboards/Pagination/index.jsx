662074db794835a504d4a6a918045cac9409d0a8
Aman Patidar <aman.patidar@synergytop.com>
2023-08-01 13:13:19 +0530

    remove mui

--- machadalo/src/Dashboards/Pagination/index.jsx
+++ machadalo/src/Dashboards/Pagination/index.jsx
@@ -25,3 +25,23 @@ const Paginations = (props) => {
 };
 
 export default Paginations;
+
+// import React from 'react';
+// import ReactPaginate from 'react-paginate';
+
+// const Paginations = (props) => {
+//   const { pageNo, pageSize, totalItems, onPageChange } = props;
+//   const numOfPages = Math.ceil(totalItems / pageSize);
+//   if (numOfPages <= 1) return null;
+//   return (
+//     <ReactPaginate
+//       pageCount={numOfPages}
+//       initialPage={pageNo}
+//       onPageChange={onPageChange}
+//       activeClassName={'active'}
+//       disableInitialCallback={true}
+//     />
+//   );
+// };
+
+// export default Paginations;
