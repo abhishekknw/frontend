000202968e4afb635c369eeeb03ade53dbe61793
contact@taletree.app <contact@taletree.app>
2023-08-02 17:43:52 +0530

    worked on relation modal and other modal

--- machadalo/src/Dashboards/Pages/BookingPlan/RelationshipModal.jsx
+++ machadalo/src/Dashboards/Pages/BookingPlan/RelationshipModal.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
 export default function RelationshipModal() {
-  return <div>RelationshipModal</div>;
+  return (
+  <>
+    <div className='rdiv'> 
+      <ul>
+        <li><h4 className='relationhead'>Supply Owner / Representative</h4></li>
+        <li className='relationdata'></li>
+      </ul>
+      <ul>
+        <li><h4 className='relationhead'>Feedback About Supplier</h4></li>
+        <li className='relationdata'>Not Given</li>
+      </ul>
+      <ul>
+        <li><h4 className='relationhead'>Last Campaign Price With Name	</h4></li>
+        <li className='relationdata'>-</li>
+      </ul>
+      <ul>
+        <li><h4 className='relationhead'>Last Campaign Price per unit </h4></li>
+        <li className='relationdata'></li>
+      </ul>
+      <ul>
+        <li><h4 className='relationhead'>Count Of Previous Campaigns</h4></li>
+        <li className='relationdata'>0</li>
+      </ul>
+
+
+    </div>
+  </>
+  );
 }
