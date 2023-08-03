044c86e5d91edf17861ef8662d06fd8e42a32d84
Aman Patidar <aman.patidar@synergytop.com>
2023-08-03 13:20:53 +0530

    remove mui

--- machadalo/src/Dashboards/common/date-range-filter/dateFilter.jsx
+++ machadalo/src/Dashboards/common/date-range-filter/dateFilter.jsx
@@ -1,4 +1,3 @@
-// import { Box } from '@mui/material';
 import React from 'react';
 import { BsFillCalendarDateFill, BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import Row from 'react-bootstrap/Row';
