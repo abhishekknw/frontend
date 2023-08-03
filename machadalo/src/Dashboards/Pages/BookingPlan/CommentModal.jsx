87ebf9eafc50bd8d73c4830f74a5a740c06ff16c
contact@taletree.app <contact@taletree.app>
2023-08-03 18:45:23 +0530

    worked on import modal

--- machadalo/src/Dashboards/Pages/BookingPlan/CommentModal.jsx
+++ machadalo/src/Dashboards/Pages/BookingPlan/CommentModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsFillPersonFill } from "react-icons/bs";
+import Form from 'react-bootstrap/Form';
 
 export default function CommentModal() {
   return (
@@ -25,6 +26,15 @@ export default function CommentModal() {
           <div className='cboxinner d-flex'>
             <span className='me-3'><BsFillPersonFill /></span>  <span><span className='comment-author'>vidhidevelopment :</span><span className='comment-time'> Sep 16, 2020 8:17:58 PM</span>
               <p className='comment-text'>Test</p></span>
+          </div>
+        </div>
+        <div>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Control as="textarea" placeholder='Write here' rows={3} />
+          </Form.Group>
+
+          <div>
+            <button className='btn me-3 btn-primary'>Add Comments</button>
           </div>
         </div>
       </div>
--- machadalo/src/Dashboards/Pages/BookingPlan/ImportSheetModal.jsx
+++ machadalo/src/Dashboards/Pages/BookingPlan/ImportSheetModal.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form';
 export default function ImportSheetModal() {
   return  (
     <>
-    <div>ImportSheetModal</div>;
+    <div>
+      <Form.Group controlId="formFile" className="mb-3">
+        {/* <Form.Label>Default file input example</Form.Label> */}
+        <Form.Control type="file" />
+      </Form.Group>
+      <a href="#">Sample Sheet</a>
+      <div className='mt-4'>
+        <button className='btn btn-primary'>Submit</button>
+      </div>
+    </div>
     
     </>
     );
