706d3e6095eb4d337ba2f886fc5f7c9c72feb0cd
Aman Patidar <aman.patidar@synergytop.com>
2023-08-07 16:58:18 +0530

    style to react select

--- machadalo/src/Dashboards/common/SelectDropdown/SelectDropdown.jsx
+++ machadalo/src/Dashboards/common/SelectDropdown/SelectDropdown.jsx
@@ -8,7 +8,7 @@ export default function SelectDropdown(props) {
   return (
     <div>
       <Select
-        className={className}
+        className={'react-select-container'}
         label={label}
         id={id}
         placeholder={placeholder}
@@ -17,6 +17,21 @@ export default function SelectDropdown(props) {
         onChange={(e) => {
           handleSelect(e, rowData);
         }}
+        styles={{
+          control: (baseStyles, state) => ({
+            ...baseStyles,
+            borderColor: state.isFocused ? '' : '',
+          }),
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary25: 'rgb(178, 212, 255)',
+            primary: '#3e59e3',
+          },
+        })}
       />
     </div>
   );
