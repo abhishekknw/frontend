9233d9165f4989e30d6c73395734b6cd181c46e9
contact@taletree.app <contact@taletree.app>
2023-08-16 13:31:58 +0530

    design payment method modaL

--- machadalo/src/Dashboards/Pages/BookingPlan/PaymentTypeModal.jsx
+++ machadalo/src/Dashboards/Pages/BookingPlan/PaymentTypeModal.jsx
@@ -4,13 +4,27 @@ import Form from 'react-bootstrap/Form';
 export default function PaymentTypeModal() {
   return (
     <>
-      testign
-      <Form.Group controlId="formFile" className="mb-3">
-        <Form.Control type="file" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control as="textarea" placeholder="Enter comments for permission box" rows={3} />
-      </Form.Group>
+      <h6>Check Payment Details, Edit As per requirement and send to Account</h6>
+      <div>
+        {/* <Form.Group controlId="formFile" className="mb-3">
+          <Form.Control type="file" />
+        </Form.Group> */}
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control placeholder="Beneficiary Name"  />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Control placeholder="Bank Account Number"  />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Control placeholder="IFSC Code"  />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Control placeholder="Negotiated Price"  />
+        </Form.Group>    <Form.Group className="mb-3" controlId="">
+          <Form.Control placeholder="Message"  />
+        </Form.Group>
+        <button className='btn btn-primary'>Send</button>
+      </div>
     </>
   );
 }
