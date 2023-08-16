236831c4be1ebacd07d19ff5b31bf5af77b62aff
contact <contact@taletree.app>
2023-08-16 12:42:09 +0530

    added routes

--- machadalo/src/constants/routes.constants.js
+++ machadalo/src/constants/routes.constants.js
@@ -4,7 +4,9 @@ export const MACHADALO_DASHBOARD_ROUTE = '/r/dashboard/machadalo';
 export const CLIENT_DASHBOARD_ROUTE = '/r/dashboard/client';
 
 //Manage Supplier
-export const MANAGE_SUPPLIER = '/r/manageSupplier';
+export const MANAGE_SUPPLIER = '/r/manageSupplier/';
+export const DASHBOARD_SUPPLIER = '/r/manageSupplier/dashboard';
+export const CREATE_SUPPLIER = '/r/manageSupplier/create';
 
 export const ROUTESNAME = {
   CAMPAIGN_DECISION_BOARD: '/campaignDecisionBoard',
