e2d53798271a2951d81b62d99f81fdc80993cd68
Aman Patidar <amanpatidar@synergytop.com>
2023-09-04 16:45:57 +0530

    set mobile number in object when ops dash modal is open

--- ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
+++ ui/buyer_catalogue/app/modules/pages/covid/templateDashboard/callUserDetails/callUserDetail.js
@@ -265,6 +265,7 @@ angular
           }
 
           $scope.showHideIntervene = function (data) {
+            data = data ? data : { 'phone_number': $scope.formData.phone_number };
             $('#onCallUserDetails').modal('hide');
             $scope.interveneDashboard = { ...$scope.interveneDashboard, show: !$scope.interveneDashboard.show, data: data };
           }
